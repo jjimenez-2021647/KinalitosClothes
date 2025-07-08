48b924f9b5facc785cdc6fcf35825dd8045d5b79
jjimenez-2021647 <jjimenez-2021647@kinal.edu.gt>
2025-07-07 21:12:36 -0600

    feat: Creacion de la base de datos en el archivo .sql

--- DB_KinalitosClothes.sql
+++ DB_KinalitosClothes.sql
@@ -1,1 +1,11 @@
-"Base de datos" 
+-- Por Josué Gilberto Jiménez Ajtún
+-- 2021647
+-- IN5BM
+-- 07/07/2025
+
+-- Creación de la base de datos
+Drop Database if exists DB_KinalitosClothes; 
+Create Database DB_KinalitosClothes;
+Use DB_KinalitosClothes;
+
+-- ---------- Entidades ----------
